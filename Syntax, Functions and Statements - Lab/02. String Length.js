f16832ccb276451c725609b696bf10235f10bf59
Petar Mitrevichin <pmitrevichin@gmail.com>
2023-09-24 12:52:43 +0300

    Add files via upload

---
+++ Syntax, Functions and Statements - Lab/02. String Length.js
@@ -0,0 +1,9 @@
+function solve(str1, str2, str3) {
+
+    let length = str1.length + str2.length + str3.length;
+    console.log(length);
+    console.log(Math.floor((length / 3)));
+
+}
+
+solve('chocolate', 'ice cream', 'cake');
