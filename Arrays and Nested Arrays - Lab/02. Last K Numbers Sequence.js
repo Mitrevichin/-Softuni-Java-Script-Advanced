d70c5e339d15236a2668e8fa4eb89a795ea4cd96
Petar Mitrevichin <pmitrevichin@gmail.com>
2023-10-04 18:52:27 +0300

    Add files via upload

---
+++ Arrays and Nested Arrays - Lab/02. Last K Numbers Sequence.js
@@ -0,0 +1,18 @@
+function kSequence(n, k) {
+    let result = [1];
+
+    for (let i = 0; i < n - 1; i++) {
+        let lastK = result.slice(-k);
+
+        let sum = 0;
+
+        for (let element of lastK) {
+            sum += element;
+        }
+
+        result.push(sum);
+    }
+    return result;
+}
+
+kSequence(6, 3);
