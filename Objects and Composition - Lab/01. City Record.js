2fbd196b82d47efdd626431e949237a2a3fb844c
Petar Mitrevichin <pmitrevichin@gmail.com>
2023-10-24 14:10:29 +0300

    Add files via upload

---
+++ Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,13 @@
+function solve(name, population, treasury) {
+    let city = {
+        name,
+        population,
+        treasury,
+    }
+    return city;
+}
+
+solve('Tortuga',
+    7000,
+    15000
+);
